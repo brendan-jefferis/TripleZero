b037ea5e093a4023234c8135db22fd62d8401035
Brendan Jefferis <brendan.jefferis@gmail.com>
2018-02-25 12:32:21 +1100

    Client mobile app sends profile on connect; added call-taker dashboard with basic caller-list functionality

--- src/index.js
+++ src/index.js
@@ -5,25 +5,70 @@ import Router from 'koa-router'
 import route from 'koa-route'
 import logger from 'koa-logger'
 import websockify from 'koa-websocket'
+import events from 'events'
 
 const port = 3000
 const app = websockify(new Koa())
 const httpRouter = new Router()
 
+const connections = {}
+const connectionEvents = new events.EventEmitter()
+
+const readyStates = {
+  CONNECTING: 0,
+  OPEN: 1,
+  CLOSING: 2,
+  CLOSED: 3
+}
+
+function addConnection(id, profile) {
+  connections[id] = profile
+  connectionEvents.emit('update')
+  console.log(`Added connection for ${id}`)
+}
+
+function removeConnection(id) {
+  delete connections[id]
+  connectionEvents.emit('update')
+  console.log(`Removed connection for ${id}`)
+}
+
 httpRouter.get('/', async ctx => {
-  ctx.body = 'hello'
+  ctx.body = connections
 })
+
+app.ws.use(route.all('/dashboard', ctx => {
+  console.log('dashbopard readt satata', ctx.websocket.readyState)
+  if (ctx.websocket.readyState === readyStates.OPEN) ctx.websocket.send(JSON.stringify(connections))
+  connectionEvents.on('update', () => {
+    if (ctx.websocket.readyState === readyStates.OPEN) ctx.websocket.send(JSON.stringify(connections))
+  })
+}))
 
 app.ws.use((ctx, next) => next(ctx))
 
 app.ws.use(route.all('/test/:id', (ctx, id) => {
   console.log(`User ${id} | /test | CONNECTED`)
   ctx.websocket.on('message', message => {
-    console.log(`User ${id} | /test | SENT ${message}`)
+    const data = JSON.parse(message)
+    if (data.profile) {
+      addConnection(id, data.profile)
+      ctx.websocket.send(JSON.stringify({
+        id: `${id}:${Date.now()}`,
+        message: `CONNECTED`
+      }))
+      return
+    }
     ctx.websocket.send(JSON.stringify({
       id: `${id}:${Date.now()}`,
-      message: `echo ${message}`
+      message: `echo ${data.message}`
     }))
+    console.log(connections)
+  })
+
+  ctx.websocket.on('close', () => {
+    console.log(`USER ${id} | /test | CLOSED`)
+    removeConnection(id)
   })
 }))
 
